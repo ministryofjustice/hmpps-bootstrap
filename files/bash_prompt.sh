b3781cd2d7fbc2f657c1593967fbc2e381c23041
Brett Minnie <github@bdb-studios.co.uk>
2018-07-11 09:03:21 +0100

    Some debug

--- files/bash_prompt.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ${EUID} == 0 ]]
-then
-        PS1="\[\033[01;34m\]({{ lookup('env', 'HMPPS_STACK') }}) \\\u@{{ lookup('env', 'HMPPS_ROLE') }}: #"
-else
-        PS1="\[\033[01;31m\]({{ lookup('env', 'HMPPS_STACK') }}) \\\u@{{ lookup('env', 'HMPPS_ROLE') }}: $"
-fi
